ac9625fa8c2129b6299d983e45426044d8499fdc
Marcin Żuk <Gnoneks@gmail.com>
2023-07-12 14:27:07 +0200

    Add header & display apps list

---
+++ app/components/apps-list/Apps-list.tsx
@@ -0,0 +1,9 @@
+import AppCard from "./App-card";
+
+export default function AppsList() {
+  return (
+    <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center gap-4 p-6">
+      <AppCard appName={'Sugoma'} appDescription={'App about sugoma that is sussy lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '}/>
+    </div>
+  );
+}
---
+++ app/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className="flex justify-center p-6 min-w-full bg-slate-600">
+      <h1 className="text-3xl tracking-wide text-shadow text-white">My React Playground</h1>
+    </div>
+  );
+}
