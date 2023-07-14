dc26324ccb518c87d7fd77bf9fa0f8e9f588c0ad
Marcin Żuk <Gnoneks@gmail.com>
2023-07-14 10:26:47 +0200

    Added more tools to header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,9 +1,48 @@
-import './header.css'
+"use client";
+import { useRouter } from "next/navigation";
+import "./header.css";
+import Link from "next/link";
 
 export default function Header() {
+  const router = useRouter();
+
+
+
   return (
-    <div className="flex justify-center p-6 min-w-full bg-slate-600">
-      <h1 className="text-3xl tracking-wide text-shadow text-white">My React Playground</h1>
+    <div className="flex justify-around p-6 min-w-full bg-slate-600">
+      <Link className="left-48" href={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-9 h-9"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </Link>
+      <h1 className="text-3xl tracking-wide text-shadow text-white">
+        My React Playground
+      </h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-9 h-9"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
     </div>
   );
 }
